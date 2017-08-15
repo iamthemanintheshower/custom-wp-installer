1ee66ea147d5fd647fa5fe08259499089939df56
Fabio <imthemanintheshower@gmail.com>
2017-08-16 01:57:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+/*
+Copyright 2017 iamthemanintheshower@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished 
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+*/
+
 include 'conf/app_config.php';
 include 'conf/config.php';
 $status = '';
@@ -185,13 +207,8 @@ if(isset($_POST)
                     <div class="row">
                         <div class="col-md-12">
                             <p>
-                                <b>Dropped files:</b><br>
-                                – xmlrpc.php<br>
-                                – wp-trackback.php<br>
-                                – wp-signup.php<br>
-                                – wp-links-opml.php<br>
-                                – wp-cron.php<br>
-                                – wp-comments-post.php
+                                <b>WP Template:</b><br>
+                                – put your favorite WP instance under _template-hsdk/WP-template folder<br>
                             </p>
                         </div>
                     </div>
